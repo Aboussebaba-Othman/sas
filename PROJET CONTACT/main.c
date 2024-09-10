708cf176b6a72f3278a807b481d828c2857572b5
aboussebaba <O.aboussebaba@hotmail.com>
2024-09-10 14:13:38 +0200

    changing

--- PROJET CONTACT/main.c
+++ PROJET CONTACT/main.c
@@ -1,51 +1,134 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-struct contact{
-    char nom[50];
-    char num_tel[50];
-    char email[50];
-
+struct contact {
+    char nom[100];
+    int numero;
+    char email[100];
+    int ID;
 };
+struct contact st[100];
+
+// Global variables
+int nbr_contact = 0;
+int choix;
 struct contact table[100];
-int nbr_contact =0;
-void ajeuter_contact(){
-    printf("entre les informations de contact\n");
-    printf("entre le nom de contact:\t");
-    scanf(" %[^\n]",table[nbr_contact].nom);
-    printf("entre le num de contact:\t");
-    scanf(" %[^\n]",table[nbr_contact].num_tel);
-    printf("entre email:\t");
-    scanf(" %[^\n]",table[nbr_contact].email);
+char nomContact_modifier[100];
+int lastItem = 0;
 
+// Function to add a new contact
+void ajouter_contact() {
+    printf("---- Ajouter un contact ----\n");
+    printf("Tapez le nom: \t");
+    scanf(" %[^\n]s", table[nbr_contact].nom);
+
+    printf("Tapez le numéro: \t");
+    scanf("%d", &table[nbr_contact].numero);
+
+    printf("Tapez l'email: \t");
+    scanf(" %[^\n]s", table[nbr_contact].email);
+
+    table[nbr_contact].ID = 123 + lastItem;
+    lastItem++;
+    nbr_contact++;
 }
-void Supprimer_contact(){
-    int contact_charch;
-    printf("entre le nom de contact");
 
+// Function to modify a contact
+void modifier_contact() {
+    int count = 0, x;
 
-}
+    printf("Entrez le nom du contact à modifier: \t");
+    scanf(" %[^\n]s", nomContact_modifier);
 
-int main()
-{
-     int choix;
-    do{
-        printf("------- MENU-----------\n");
-        printf("1 : Ajouter un Contact\n");
-        printf("2 : Modifier un Contact\n");
-        printf("3 : Supprimer un Contact\n");
-        printf("4 : Afficher Tous les Contacts \n");
-        printf("5 : Rechercher un Contact\n");
-        printf(" choisir votre choix :\n");
-        scanf("%d",&choix);
-
-        switch(choix){
-        case 1:
-            ajeuter_contact();
-            break;
+    for (int i = 0; i < nbr_contact; i++) {
+        if (strcmp(nomContact_modifier, table[i].nom) == 0) {
+            printf("-----------------------\n");
+            printf("Nom: %s\n", table[i].nom);
+            printf("Numéro: %d\n", table[i].numero);
+            printf("Email: %s\n", table[i].email);
+            printf("ID: %d\n", table[i].ID);
+            count++;
         }
     }
-        while(!choix);
+
+    if (count > 1) {
+        printf("Entrez l'ID: \t");
+        scanf("%d", &x);
+
+        for (int i = 0; i < nbr_contact; i++) {
+            if (table[i].ID == x) {
+                printf("Modifier le contact:\n");
+                printf("Nom: \t");
+                scanf(" %[^\n]s", table[i].nom);
+                printf("Email: \t");
+                scanf(" %[^\n]s", table[i].email);
+                printf("Numéro: \t");
+                scanf("%d", &table[i].numero);
+                printf("*******************************\n");
+                return;
+            }
+        }
+    } else if (count == 1) {
+        for (int i = 0; i < nbr_contact; i++) {
+            if (strcmp(nomContact_modifier, table[i].nom) == 0) {
+                printf("Modifier le contact:\n");
+                printf("Nom: \t");
+                scanf(" %[^\n]s", table[i].nom);
+                printf("Email: \t");
+                scanf(" %[^\n]s", table[i].email);
+                printf("Numéro: \t");
+                scanf("%d", &table[i].numero);
+                printf("*******************************\n");
+                return;
+            }
+        }
+    } else {
+        printf("Aucun contact trouvé!!!!!\n");
+    }
+}
+
+// Function to display all contacts
+void affichage() {
+    printf("------- Affichage des contacts --------\n");
+    for (int i = 0; i < nbr_contact; i++) {
+        printf("-------------------\n");
+        printf("Nom: %s \n", table[i].nom);
+        printf("Numéro: %d \n", table[i].numero);
+        printf("Email: %s \n", table[i].email);
+    }
+}
+
+int main() {
+    do {
+        printf("----- Acceuil --------\n");
+        printf("1) Ajouter un contact...\n");
+        printf("2) Modifier un contact...\n");
+        printf("3) Supprimer un contact...\n");
+        printf("4) Afficher un contact...\n");
+        printf("5) Chercher un contact...\n");
+        printf("0) Quitter\n");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                ajouter_contact();
+                break;
+            case 2:
+                modifier_contact();
+                break;
+            case 3:
+                // Ajouter logique pour supprimer le contact (non implémenté)
+                break;
+            case 4:
+                affichage();
+                break;
+            default:
+                if (choix != 6) {
+                    printf("Oopps, choix invalide!\n");
+                }
+                break;
+        }
+    } while (choix != 6);
+
     return 0;
 }
